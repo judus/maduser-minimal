7f1de9d45346b5e50ef2f04d768576660fd2f0dd
Julien Duseyau <julien.duseyau@gmail.com>
2017-07-11 01:34:18 +0200

    Implemented handling of flat arrays

--- src/Minimal/Core/Config.php
+++ src/Minimal/Core/Config.php
@@ -13,28 +13,102 @@ class Config implements ConfigInterface
 	/**
 	 * @var array
 	 */
-	protected $config = [];
+	protected $items = [];
 
     /**
-     * @param      $name
-     * @param null $value
+     * @var bool
+     */
+    protected $literal = false;
+
+    /**
+     * @return array
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param array $items
+     *
+     * @return Config
+     */
+    public function setItems(array $items): Config
+    {
+        $this->items = $items;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLiteral(): bool
+    {
+        return $this->literal;
+    }
+
+    /**
+     * @param bool $literal
+     *
+     * @return Config
+     */
+    public function setLiteral(bool $literal): Config
+    {
+        $this->literal = $literal;
+
+        return $this;
+    }
+
+    /**
+     * @param           $name
+     * @param null      $value
+     * @param null|bool $literal
      *
      * @return mixed
      * @throws KeyDoesNotExistException
      */
-    public function item($name, $value = NULL)
+    public function item($name, $value = null, $literal = null)
     {
-	    if (func_num_args() > 1) {
-            $this->config[$name] = $value;
+        $literal = !is_null($literal) ? $literal : $this->isLiteral();
+
+        if (func_num_args() > 1) {
+            $this->items[$name] = $value;
         }
 
-        if (!isset($this->config[$name])) {
-            throw new KeyDoesNotExistException(
-                'Config key \''.$name.'\' does not exist',
-                ['Config' => $this->config]
-            );
+        if (!$literal) {
+            return $this->find($name, $this->items);
         }
 
-		return $this->config[$name];
-	}
+        isset($this->items[$name]) || $this->throwKeyDoesNotExist($name);
+
+        return $this->items[$name];
+    }
+
+    /**
+     * @param      $name
+     * @param      $array
+     * @param null $parent
+     *
+     * @return mixed
+     */
+    protected function find($name, $array, $parent = null)
+    {
+        list($key, $child) = array_pad(explode('.', $name, 2), 2, null);
+        isset($array[$key]) || $this->throwKeyDoesNotExist($name);
+        return $child ? $this->find($child, $array[$key], $name) : $array[$key];
+    }
+
+    /**
+     * @param $name
+     *
+     * @throws KeyDoesNotExistException
+     */
+    protected function throwKeyDoesNotExist($name)
+    {
+        throw new KeyDoesNotExistException(
+            'Config key \'' . $name . '\' does not exist',
+            ['Config' => $this->items]
+        );
+    }
 }
