f91f07c762570a3dd9d698e618aad28ffd1a3b96
Julien Duseyau <julien.duseyau@gmail.com>
2017-07-22 01:28:33 +0200

    Collections are now Iterators

--- src/Minimal/Collections/Collection.php
+++ src/Minimal/Collections/Collection.php
@@ -9,7 +9,7 @@ use Maduser\Minimal\Collections\CollectionInterface;
  *
  * @package Maduser\Minimal\Collections
  */
-class Collection implements CollectionInterface
+class Collection implements CollectionInterface, \Iterator
 {
 	/**
 	 * @var array
@@ -87,6 +87,24 @@ class Collection implements CollectionInterface
 		}
 	}
 
+
+	public function extract($key)
+    {
+        $extracted = [];
+
+        foreach ($this->items as $item) {
+            foreach (func_get_args() as $key) {
+                if (is_object($item)) {
+                    $extracted[] = $item->{$key};
+                } else {
+                    $extracted[] = $item[$key];
+                }
+            }
+        }
+
+        return $extracted;
+    }
+
 	/**
 	 * @return array
 	 */
@@ -95,4 +113,60 @@ class Collection implements CollectionInterface
 		return $this->items;
 	}
 
+    /**
+     * Return the current element
+     *
+     * @link  http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return current($this->items);
+    }
+
+    /**
+     * Move forward to next element
+     * @link  http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        return next($this->items);
+    }
+
+    /**
+     * Return the key of the current element
+     * @link  http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return key($this->items);
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link  http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return $this->current() !== false;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link  http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        reset($this->items);
+    }
 }
